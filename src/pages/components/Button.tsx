5ecb2ea451e717db3921be7305db8526fa1e1831
mi <musailliasu7@gmail.com>
2025-01-11 23:11:46 +0100

    hero section UI

--- src/pages/components/Button.tsx
+++ src/pages/components/Button.tsx
@@ -1,16 +1,22 @@
+import { Play } from "@phosphor-icons/react";
 import { Link } from "react-router-dom"
 
-const Button = () => {
+interface Props {
+  icon?: boolean;
+}
+
+const Button = ({icon}: Props) => {
 
   return (
-    <>
+    <div className="w-max">
         <Link to="#" className="btn font-semibold flex justify-center items-center gap-1 rounded-full bg-white text-black hover:bg-blue-500 hover:text-white transition-colors duration-300 py-3 px-6 z-10">
             <span>Book a Call</span>
+            {icon && <Play size={16} weight="fill" />}
         </Link>
         <span className="w-full inline-block font-semibold rounded-full absolute -left-[0.2rem] -bottom-[0.4rem] text-xs text-transparent select-none border-2 border-dotted transition-all duration-300 py-3 px-6 z-0">
             ""
         </span>
-    </>
+    </div>
   )
 }
 
